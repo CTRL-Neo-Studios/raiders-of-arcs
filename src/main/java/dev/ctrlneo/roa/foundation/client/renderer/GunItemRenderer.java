c92f4e077a8cd64ee917458ba5a63098b19f9f31
Type-32 <87076491+Type-32@users.noreply.github.com>
2025-10-27 21:05:08 +0800

    chore: seems like the azure lib hides bones named "leftArm" and "rightArm" by default

--- src/main/java/dev/ctrlneo/roa/foundation/client/renderer/GunItemRenderer.java
+++ src/main/java/dev/ctrlneo/roa/foundation/client/renderer/GunItemRenderer.java
@@ -1,10 +1,16 @@
 package dev.ctrlneo.roa.foundation.client.renderer;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import dev.ctrlneo.roa.foundation.animations.animators.GunItemAnimator;
 import dev.ctrlneo.roa.foundation.utils.Reference;
 import mod.azure.azurelib.common.render.item.AzItemRenderer;
 import mod.azure.azurelib.common.render.item.AzItemRendererConfig;
+import mod.azure.azurelib.common.render.item.AzItemRendererPipeline;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
 public class GunItemRenderer extends AzItemRenderer {
     private final ResourceLocation GEO, TEX;
